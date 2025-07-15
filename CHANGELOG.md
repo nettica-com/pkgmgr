38a99bdc5bb2205e24b807c4216cf1ee6d3e35c9
Alan Graham <alan@nettica.com>
2025-07-15 15:14:47 -0700

    Release v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.1
+* Fix repository URL, dart format
+
 ## 1.0.0
 
 * Initial Release
--- lib/pkgmgr.dart
+++ lib/pkgmgr.dart
@@ -2,7 +2,7 @@ import 'pkgmgr_platform_interface.dart';
 
 ///
 /// Represents information about an installed app.
-/// 
+///
 class AppInfo {
   final String name;
   final String packageId;
@@ -24,11 +24,11 @@ class Pkgmgr {
     return PkgmgrPlatform.instance.getPlatformVersion();
   }
 
-/// Returns a list of all installed apps on the device.  Requires QUERY_ALL_PACKAGES permission.
-///
-/// If [showSystem] is true, system apps are included.
-/// Each app includes its name, packageId, and iconBase64 (as base64).
-/// See example for typical usage.
+  /// Returns a list of all installed apps on the device.  Requires QUERY_ALL_PACKAGES permission.
+  ///
+  /// If [showSystem] is true, system apps are included.
+  /// Each app includes its name, packageId, and iconBase64 (as base64).
+  /// See example for typical usage.
   Future<List<AppInfo>> getInstalledApps({bool showSystem = false}) async {
     final apps = await PkgmgrPlatform.instance.getInstalledApps(
       showSystem: showSystem,
